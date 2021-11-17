ab66e879194437e50fb44b67c9bb2adc627c8fc5
Jim Wooley <jimwooley@hotmail.com>
2021-11-17 13:42:02 -0500

    Moving better interpolated strings case

---
+++ CS10/BetterInterpolatedStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace LanguageFeatures.CS10;
+
+public class BetterInterpolatedStrings
+{
+    [Fact]
+    public void TestStringInterpolation()
+    {
+        // TODO: Copy this into https://sharplab.io to see the generated difference
+        // in string interpolation with multiple parameters
+        var anon = new { First = "Jim", Last = "Wooley" };
+        Assert.Equal("Jim Wooley aka James Wooley", $"{anon.First} {anon.Last} aka James {anon.Last}");
+    }
+
+}
--- CS10/WithExpressionsAnonymousTypes.cs
+++ CS10/WithExpressionsAnonymousTypes.cs
@@ -9,13 +9,4 @@ public class WithExpressionsAnonymousTypes
         var legal = anon with { First = "James" };
         Assert.Equal("James Wooley", $"{legal.First} {legal.Last}");
     }
-
-    [Fact]
-    public void BetterInterpolatedStrings()
-    {
-        // TODO: Copy this into https://sharplab.io to see the generated difference
-        // in string interpolation with multiple parameters
-        var anon = new { First = "Jim", Last = "Wooley" };
-        Assert.Equal("Jim Wooley aka James Wooley", $"{anon.First} {anon.Last} aka James {anon.Last}");
-    }
 }

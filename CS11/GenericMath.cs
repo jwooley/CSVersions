2f31493af0d6199c5c61d5e0385f5e9c2ce68f77
JIMXPS13\jimwo <jimwooley@hotmail.com>
2022-11-14 23:08:41 -0500

    Add pattern matching tail recursion version of sum in generic math

--- CS11/GenericMath.cs
+++ CS11/GenericMath.cs
@@ -13,10 +13,10 @@ namespace LanguageFeatures.CS11
         [Fact]
         public void CanAddNums()
         {
-            var ints = new int[] { 1, 2, 3 };
-            var decimals = new decimal[] { 1.1M, 1.2M, 1.3M };
-            var doubles = new double[] { 1.1, 1.2, 1.3 };
-            var floats = new float[] { 1.1f, 1.2f, 1.3f };
+            var ints = new [] { 1, 2, 3 };
+            var decimals = new [] { 1.1M, 1.2M, 1.3M };
+            var doubles = new [] { 1.1, 1.2, 1.3 };
+            var floats = new [] { 1.1f, 1.2f, 1.3f }.AsSpan();
 
             Assert.Equal(6, Sum(ints));
             Assert.Equal(3.6M, Sum(decimals));
@@ -42,5 +42,14 @@ namespace LanguageFeatures.CS11
 
             return result;
         }
+
+        public static T Sum<T>(Span<T> values)
+            where T : INumber<T> => 
+            values switch
+            {
+                [] => T.Zero,
+                [var first, .. var rest] => first + Sum(rest),
+            };
+
     }
-}
+}           
